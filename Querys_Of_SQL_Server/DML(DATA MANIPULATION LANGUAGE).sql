a01a2898c679e4597be7aa06530347eb09fd5267
COxRIPMIZO <98731652+COxRIPMIZO@users.noreply.github.com>
2022-09-26 14:11:27 +0530

    Add files via upload

---
+++ Querys_Of_SQL_Server/DML(DATA MANIPULATION LANGUAGE).sql
@@ -0,0 +1,67 @@
+﻿--                                            SQL 
+--                                              |
+--                                              |
+--						-------------------           -------------------
+--						|		   		   |		  |					 |
+--					   DML				  DDL		 DQL				DCL
+--						|				   |		  |					 |
+--					  INSERT			 CREATE		SELECT			   GRANT
+--						|    			   |							 |
+--					  UPDATE			  DROP						   REVOKE
+--						|				   |
+--					  DELETE			 ALTER
+--										   |
+--										TRUNCATE
+
+
+
+-- DML(DATA MANIPULATION LANGUAGE)
+--COMMAND 1 - INSERT - USED FOR INSERTING DATA IN TABLE OR WE SAY THAT IT IS USED FOR INSERTING NEW ROW  IN TABLE
+--SYNTAX
+--INSERT INTO <TABLE NAME>(<ALL COLUNM NAME LIKE(COLUNM  1 , COLUNM 2...)>) VALUES(VALUES FOR THAT RESPECTIVE COLUNMS)
+--i.e
+INSERT INTO TEST (NAME , ADDRESS , PHONE) VALUES("VISHAL" , "SANTACRUZ" , "1234567890")
+--NOTE:- ALWAYS REMEBER NUMBER OF COLUMN IS EQUAL TO NUMMBER OF VALUES
+--
+
+
+
+--COMMAND 2 - UPDATE - USED FOR  CHANGING OR MODIFY TABLE DATA OR COLUNM DATA 
+--SYNTAX
+--UPDATE <TABLE NAME>  SET <COLUNM NAME> = <VALUES> WHERE <CONDITIONS>
+--i.e
+UPDATE TEST_TABLE SET NAME = "PRAVIN SIR" WHERE ID = 1;
+--
+
+
+
+
+--COMMAND 3 - DELETE - USED FOR DELETING ROWS OR DATA FROM TABLE
+--SYNTAX
+--DELETE FROM TABLE <TABLE NAME> WHERE  <CONDITION>
+--i.e
+DELETE FROM TABLE TEST_TABLE WHERE ID = 2;
+--
+
+-------------------------------------------KHONSHU(Vishal)----------------------------------------------
+--                                        😊😊😊😊👌👌👌👌
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Querys_Of_SQL_Server/DQL(DATA QUREY LANGUAGE).sql
@@ -0,0 +1,43 @@
+﻿--                                             SQL 
+--                                              |
+--                                              |
+--						-------------------           -------------------
+--						|		   		   |		  |					 |
+--					   DML				  DDL		 DQL				DCL
+--						|				   |		  |					 |
+--					  INSERT			 CREATE		SELECT			   GRANT
+--						|    			   |							 |
+--					  UPDATE			  DROP						   REVOKE
+--						|				   |
+--					  DELETE			 ALTER
+--										   |
+--										TRUNCATE
+
+
+--DQL (DATA QUERY LANGUAGE)
+-- SELECT - USED FOR FECHING DATA FFROM TABLE 
+--SYNTAX FOR FECHING ALL DATA FROM TABLE
+--SELECT * FROM <TABLE NAME>
+SELECT * FROM TEST_TABLE;
+--SYNTAX FOR FECHING SINGLE DATA FROM CLUNM
+--SELECT * FROM <TABLE NAME> WHERE <CONDIITON>
+SELECT * FROM TEST_TABLE WHERE ID = 2
+--SYNTAX FOR FECHING SPECIFIC DATA FROM TABLE
+--SELECT <COLUNM NAME > FROM <TABLE  NAME> WHERE <CONDITION>
+SELECT ID , ADDRESS,NAME FROM TEST_TABLE WHERE ID =2
+
+-------------------------------------------KHONSHU(Vishal)----------------------------------------------
+--                                        😊😊😊😊👌👌👌👌
+
+
+
+
+
+
+
+
+
+
+
+
+
